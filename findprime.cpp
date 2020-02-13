24791631cc5dfe44ca5796ac41cf78a64d37ded6
mmmcn <43902594+mmmcn@users.noreply.github.com>
2020-02-13 13:01:32 +0800

    线性筛
    线性筛确定素数并制表

---
+++ findprime.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+#define N 10000
+
+int flag[N+1],prime[N+1],pnum;
+int main()
+{
+    pnum=0;
+    for(int i=0;i<=N;i++)
+        flag[i]=1;      //所有数看做素数
+    for(int i=2;i<=N;i++)
+    {
+        if(flag[i]==1)
+            prime[pnum++]=i;
+        for(int j=0;j<pnum&&prime[j]*i<=N;j++)
+        {
+            flag[prime[j]*i]=0;
+            if(i%prime[j]==0)   //找到i的最小素因数s
+                break;
+        }
+    }
+    for(int i=0;i<pnum;i++)
+        (i+1)%10==0?printf("%d\n",prime[i]):printf("%d ",prime[i]);
+
+	return 0;
+}
